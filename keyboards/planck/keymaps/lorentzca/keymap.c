4ebf368b309da1996fc12cecb45cbe4887bf180b
Kota Kunishima <koutak@amazon.co.jp>
2018-09-06 08:42:10 +0900

    Define LGT_TOG
    see https://www.reddit.com/r/olkb/comments/9b7ms9/help_planck_light_simple_backlight_control/

--- keyboards/planck/keymaps/lorentzca/keymap.c
+++ keyboards/planck/keymaps/lorentzca/keymap.c
@@ -18,6 +18,12 @@
 #include "action_layer.h"
 #include "muse.h"
 
+#ifdef KEYBOARD_planck_light
+  #define LGT_TOG RGB_TOG
+#else
+  #define LGT_TOG BL_TOGG
+#endif
+
 extern keymap_config_t keymap_config;
 
 enum planck_layers {
@@ -52,14 +58,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Esc  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |BLTOGG| Brite| Alt  | GUI  |Lower |Space |Enter |Raise | Left | Down |  Up  |Right |
+ * |LGTTOG| Brite| Alt  | GUI  |Lower |Space |Enter |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,                KC_T,    KC_Y,    KC_U,                KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,                KC_G,    KC_H,    KC_J,                KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,                KC_B,    KC_N,    KC_M,                KC_COMM, KC_DOT,  KC_SLSH, KC_ESC ,
-    BL_TOGG, BACKLIT, KC_LALT, KC_LGUI, LT(_LOWER,KC_LANG2), KC_SPC,  KC_ENT,  LT(_RAISE,KC_LANG1), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    LGT_TOG, BACKLIT, KC_LALT, KC_LGUI, LT(_LOWER,KC_LANG2), KC_SPC,  KC_ENT,  LT(_RAISE,KC_LANG1), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Colemak
