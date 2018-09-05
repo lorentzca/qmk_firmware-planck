dd4b164bc98ad03a4529156136c39fa01c41d243
Kota Kunishima <koutak@amazon.co.jp>
2018-09-05 18:44:23 +0900

    Add backlight action

--- keyboards/planck/keymaps/lorentzca/keymap.c
+++ keyboards/planck/keymaps/lorentzca/keymap.c
@@ -52,14 +52,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Esc  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | DEL  | Brite| Alt  | GUI  |Lower |Space |Enter |Raise | Left | Down |  Up  |Right |
+ * |BLTOGG| Brite| Alt  | GUI  |Lower |Space |Enter |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,                KC_T,    KC_Y,    KC_U,                KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,                KC_G,    KC_H,    KC_J,                KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,                KC_B,    KC_N,    KC_M,                KC_COMM, KC_DOT,  KC_SLSH, KC_ESC ,
-    KC_DEL,  BACKLIT, KC_LALT, KC_LGUI, LT(_LOWER,KC_LANG2), KC_SPC,  KC_ENT,  LT(_RAISE,KC_LANG1), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    BL_TOGG, BACKLIT, KC_LALT, KC_LGUI, LT(_LOWER,KC_LANG2), KC_SPC,  KC_ENT,  LT(_RAISE,KC_LANG1), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Colemak
@@ -122,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  | Left | Down |  Up  |Right |      |      |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |ISO # |ISO / |Pg Up |Pg Dn |      |
+ * |BL_INC|BL_DEC|BLBRTG|BLSTEP|      |      |      |ISO # |ISO / |Pg Up |Pg Dn |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
@@ -130,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_DEL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, _______, _______, _______, _______, _______, _______, KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
+    BL_INC,  BL_DEC,  BL_BRTG, BL_STEP, _______, _______, _______, KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
